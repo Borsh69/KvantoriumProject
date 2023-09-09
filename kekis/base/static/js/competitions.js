9e0530a00bd221e1780b23e18bbc516d50897e11
cyan1d <ggoorrooddaa@gmail.com>
2023-09-09 10:40:40 +0300

    huh

--- kekis/base/static/js/competitions.js
+++ kekis/base/static/js/competitions.js
@@ -34,40 +34,39 @@ popupToggle.onclick = function(){
       popup.style.opacity = "0";
     }
   }
-function sortList() {
+  function sortList() {
     var list, i, switching, b, shouldSwitch;
     list = document.getElementById("id01");
     switching = true;
     /*Make a loop that will continue until
     no switching has been done:*/
     while (switching) {
-        //start by saying: no switching is done:
-        switching = false;
-        b = list.getElementsByTagName("div");
-        //Loop through all list-items:
-        for (i = 0; i < (b.length - 1); i++) {
-            //start by saying there should be no switching:
-            shouldSwitch = false;
-            /*check if the next item should
-            switch place with the current item:*/
-            if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-            /*if next item is alphabetically
-            lower than current item, mark as a switch
-            and break the loop:*/
-            shouldSwitch = true;
-            break;
-            }
+      //start by saying: no switching is done:
+      switching = false;
+      b = list.getElementsByTagName("div");
+      //Loop through all list-items:
+      for (i = 0; i < (b.length - 1); i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*check if the next item should
+        switch place with the current item:*/
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+          /*if next item is alphabetically
+          lower than current item, mark as a switch
+          and break the loop:*/
+          shouldSwitch = true;
+          break;
         }
-        if (shouldSwitch) {
-            /*If a switch has been marked, make the switch
-            and mark the switch as done:*/
-            b[i].parentNode.insertBefore(b[i + 1], b[i]);
-            switching = true;
-        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark the switch as done:*/
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
     }
-}
-
-function sortListY() {
+  }
+  function sortListY() {
     var list, i, switching, b, shouldSwitch;
     list = document.getElementById("id01");
     switching = true;
@@ -100,7 +99,12 @@ function sortListY() {
     }
   }
 
-
+$(document).ready(function() {
+    $(document).on('click', '.main-block__button', function() {
+        console.log('click');
+    });
+});
+/*
 $(document).on('click', '.main-block__button', function() {
     $(this).addClass("main-block__button__liked").removeClass("main-block__button");
     const catid = $(this).attr("data-catid");
@@ -140,3 +144,5 @@ $(document).on('click', '.main-block__button__liked', function() {
         }
     });
 });
+
+*/
